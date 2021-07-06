33789ed99bd0c701c6b1bc35ada8bd72ea69f06c
Manik Sharma <maniksharma98144@gmail.com>
2021-07-06 22:37:28 +0530

    crawler initial

---
+++ src/searchEngine/Crawler.java
@@ -0,0 +1,56 @@
+package searchEngine;
+
+import java.util.*;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+public class Crawler {
+	private static final int DEPTH = 5;
+	private static final String URL = "https://en.wikipedia.org";
+	private Set<String> visitedURL = new HashSet<String>();
+	
+	public void start() {
+		crawl(1, URL);
+	}
+
+	private Document requestPage(String url) {
+		try {
+			Connection con = Jsoup.connect(url);
+			Document doc = con.get();
+			if(con.response().statusCode() == 200) {
+				System.out.println(url);
+				visitedURL.add(url);
+				return doc;
+			}
+			return null;
+		}
+		catch(Exception e) {
+			return null;
+		}
+	}
+	
+	private boolean filterURL(String link) {
+		if(link.contains(".svg") || link.contains("#") || link.contains(".png") ||
+				link.contains(".jpeg") || link.contains(".pdf") || link.contains(".gif")||
+				link.contains(".jpg") || link.contains(".swf"))
+			return false;
+		return true;
+	}
+	
+	private void crawl(int level, String url) {
+		if(level <= DEPTH) {
+			Document doc = requestPage(url);
+			if(doc!=null) {
+				for(Element link:doc.select("a[href]")) {
+					String next_link = link.absUrl("href");
+					if(!visitedURL.contains(next_link) && filterURL(next_link)) {
+						crawl(level++, next_link);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ src/searchEngine/WebSearchEngine.java
@@ -0,0 +1,9 @@
+package searchEngine;
+
+public class WebSearchEngine {
+
+	public static void main(String[] args) {
+		Crawler cr = new Crawler();
+		cr.start();
+	}
+}
