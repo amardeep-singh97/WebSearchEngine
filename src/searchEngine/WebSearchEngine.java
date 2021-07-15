e98e8a92238548ba6054031f11e766e1fe1df7a1
Manik Sharma <maniksharma98144@gmail.com>
2021-07-15 20:50:38 +0530

    minor changes

--- src/searchEngine/WebSearchEngine.java
+++ src/searchEngine/WebSearchEngine.java
@@ -4,11 +4,10 @@ import java.io.File;
 import java.io.IOException;
 
 public class WebSearchEngine {
-
 	public static void main(String[] args) throws IOException {
 		File file = new File("downloadedPages//");
 		File[] files = file.listFiles();
-		if(files == null || files.length == 0) {
+		if(files.length <= 1) {
 			System.out.println("Crawling Started... Saving web pages");
 			Crawler cr = new Crawler();
 			cr.start();
